49310a9a867a846c380b8e4372cb74e5ddb201aa
RathnapriyaNLR <rathnapriyanl27@gmail.com>
2017-06-09 14:46:54 +0530

    Create F.java

---
+++ F.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class F
+{
+public static void main(String args[])
+{
+int fat=1,n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+while(n>0)
+{
+fact=fact*n;
+}
+System.out.println("the factorial is"+fact);
+}
+}
